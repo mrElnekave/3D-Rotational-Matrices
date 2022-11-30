2afbdbacca76e5f79b64648e125d4f25c4c6723d
Yamm Elnekave <klavlavigugu@gmail.com>
2022-11-30 15:21:28 -0800

    Update try3.py

--- try3.py
+++ try3.py
@@ -52,14 +52,10 @@ def circle(xc, yc, radius, cicle_list):
                 x -= 1
                 err -= 2 * x + 1
 circle_z_plane = []
-circle_x_plane = []
-circle_y_plane = []
-circle(0, 0, 30, circle_z_plane)
-for c_point in circle_z_plane:
-    x, y, z = c_point
-    circle_x_plane.append((0, x, y))
-    circle_y_plane.append((x, 0, y))
 
+circle(0, 0, 30, circle_z_plane)
+
+points = [(0, 30, 0), (0, -30, 0), (30, 0, 0), (-30, 0, 0), (0, 0, 30), (0, 0, -30)]
 def draw_circle_z():
     first = True
     for c_point in circle_z_plane:
@@ -75,69 +71,51 @@ def draw_circle_z():
             first = False
         rb.Draw.queue_pixel(pos, color=rb.Color.red, z_index=int(z))
 
-def draw_circle_x():
-    first = True
-    for c_point in circle_x_plane:
-
-        x, y, z = c_point
-        x = get_x(x, y, z, roll, pitch, yaw)
-        y = get_y(x, y, z, roll, pitch, yaw)
-        z = get_z(x, y, z, roll, pitch, yaw)
-        pos = rb.Vector(x, y)
-        pos += center
-        if first:
-            rb.Draw.queue_circle(pos, 2, rb.Color.green, fill=rb.Color.black.lighter(int(rb.Math.map(z, -30, 30, 10, 250))))
-            first = False
-        rb.Draw.queue_pixel(pos, color=rb.Color.green, z_index=int(z))
-
-def draw_circle_y():
-    first = True
-    for c_point in circle_y_plane:
-
-        x, y, z = c_point
-        x = get_x(x, y, z, 0, pitch, yaw)
-        y = get_y(x, y, z, 0, pitch, yaw)
-        z = get_z(x, y, z, 0, pitch, yaw)
-        pos = rb.Vector(x, y)
-        pos += center
-        if first:
-            rb.Draw.queue_circle(pos, 2, rb.Color.blue, fill=rb.Color.black.lighter(int(rb.Math.map(z, -30, 30, 10, 250))))
-            first = False
-        rb.Draw.queue_pixel(pos, color=rb.Color.blue, z_index=int(z))
-
 def custom_draw():  
-    draw_circle_z()
-    draw_circle_x()
-    draw_circle_y()
+    # draw_circle_z()
+    for point in points:
+        rb.Draw.queue_circle(center, radius=2, border=None, fill=rb.Color(0, 0, 255), z_index=0)
+        x = get_x(*point, roll, pitch, yaw)
+        y = get_y(*point, roll, pitch, yaw)
+        z = get_z(*point, roll, pitch, yaw)
+        color = rb.Color.black.lighter(int(rb.Math.map(z, -30, 30, 10, 250)))
+        rb.Draw.queue_circle((x, y), fill=color, z_index=int(z))
 
-    rb.Draw.queue_circle(center, radius=2, border=None, fill=rb.Color(0, 0, 255), z_index=0)
-    x = get_x(*point, roll, pitch, yaw)
-    y = get_y(*point, roll, pitch, yaw)
-    z = get_z(*point, roll, pitch, yaw)
-    color = rb.Color.black.lighter(int(rb.Math.map(z, -30, 30, 10, 250)))
-    rb.Draw.queue_circle((x, y), fill=color, z_index=int(z))
 
 scene.draw = custom_draw
+
+text_go = rb.wrap(text:=rb.Text("Testing z rotation", anchor=(0, 1)), pos=(0, 100))
+
 
 def custom_update():
     global roll, pitch, yaw
     if rb.Input.key_pressed("a"):
+        text.text = "Testing z rotation"
         pitch += 0.01
     if rb.Input.key_pressed("d"):
         pitch -= 0.01
+        text.text = "Testing z rotation"
+
     if rb.Input.key_pressed("w"):
         roll -= 0.01
+        text.text = "Testing x rotation"
     if rb.Input.key_pressed("s"):
         roll += 0.01
+        text.text = "Testing x rotation"
     if rb.Input.key_pressed("q"):
         yaw += 0.01
+        text.text = "Testing y rotation"
     if rb.Input.key_pressed("e"):
         yaw -= 0.01
+        text.text = "Testing y rotation"
     
 
 scene.update = custom_update
 
-# rb.begin()
+scene.add_ui(text_go)
+
+
+rb.begin()
 pitch = pi
 # print("Get x: ", get_x(-1, 2, 3, 0, 0, pi / 2))
 # print("Get y: ", get_y(-1, 2, 3, 0, 0, pi / 2))
